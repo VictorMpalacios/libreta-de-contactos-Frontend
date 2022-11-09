f33682b733ca0cbee2b3432c1c21cf32d426a8da
VictorMpalacios <vmpsanchez99@gmail.com>
2022-11-08 23:25:25 -0500

    new view

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TraerContacto from './funciones/funciones';
+    
+function App() {
+  return (
+    <div className="contenedor">
+      <TraerContacto/>
+    </div>
+  );
+}
+
+export default App;
